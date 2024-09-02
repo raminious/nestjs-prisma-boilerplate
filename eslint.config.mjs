2545e7a6f0e360f4ac64333ee3f1b20da5fd0c65
Ramin Mousavi <ramin.mousavy@gmail.com>
2024-09-02 14:32:56 +0100

    upgrade eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,59 @@
+import typescriptEslintEslintPlugin from '@typescript-eslint/eslint-plugin'
+import globals from 'globals'
+import tsParser from '@typescript-eslint/parser'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+})
+
+export default [
+  {
+    ignores: ['**/.eslintrc.js'],
+  },
+  ...compat.extends(
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ),
+  {
+    plugins: {
+      '@typescript-eslint': typescriptEslintEslintPlugin,
+    },
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      parser: tsParser,
+      ecmaVersion: 5,
+      sourceType: 'module',
+      parserOptions: {
+        project: 'tsconfig.json',
+      },
+    },
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          semi: false,
+          singleQuote: true,
+          printWidth: 80,
+          trailingComma: 'all',
+          arrowParens: 'avoid',
+          endOfLine: 'auto',
+        },
+      ],
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+]
